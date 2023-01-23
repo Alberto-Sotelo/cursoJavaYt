731ae2d8c0b4c0de4923afbe7b44d454fb553175
Alberto Sotelo <irving.sotelo6543@alumnos.udg.mx>
2023-01-22 18:42:24 -0600

    Hola mundo en Java, aqui inicia el curso

---
+++ JavaYt/src/javayt/JavaYt.java
@@ -0,0 +1,18 @@
+package javayt;
+
+/**
+ *
+ * @author Alberto Sotelo
+ */
+public class JavaYt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //El codigo a mostar va aqui
+        
+        System.out.println("Hola mundo"); //Println imprime en consola 
+    }
+    
+}
