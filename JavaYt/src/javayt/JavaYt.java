f1b8d5d41f97aa673ad6f55f13a6a95d5d53046c
Alberto Sotelo <irving.sotelo6543@alumnos.udg.mx>
2023-01-23 11:26:25 -0600

    Apuntes condicionales, inician CICLOS

--- JavaYt/src/javayt/JavaYt.java
+++ JavaYt/src/javayt/JavaYt.java
@@ -1,18 +1,45 @@
 package javayt;
 
 /**
- *
+ Inicio de curso de Java en Youtube
  * @author Alberto Sotelo
  */
 public class JavaYt {
 
     /**
-     * @param args the command line arguments
-     */
+     * @param args the command line arguments */
+     
     public static void main(String[] args) {
         //El codigo a mostar va aqui
         
-        System.out.println("Hola mundo"); //Println imprime en consola 
-    }
-    
-}
+        /*
+        Almacenamiento de datos
+        Las variables serviran para almacenar numeros, caracteres
+        */
+        
+       /* int numeroEntero=1; //Aqui declaramos la variable y le asignamos un valor (para numeros enteros se usa "int")
+        
+        double numeroDoble = 2.8; //Esta variable se usa para numeros con decimales 
+        char letra = 'c'; //Para almacenar caracteres se declara la variable como "char"
+        String frase = "Hola mundo"; //Para almacenar cadenas de caracteres usaremos la variable "string"
+        
+        
+        System.out.println(letra); //Println imprime en consola  
+        */
+        
+        //Aqui comienza la clase de condicionales 
+        //Las condioionales sirven para comparar variables
+        
+        int numeroUno = 10000; 
+        int numeroDos = 1000;
+        
+        //La variable if servira para declarar condicionales, dentro de los parentesis colocaremos la condicion y dentro de las llaves
+        //lo que queremos que pase si esa condicion se cumple
+        if(numeroUno > numeroDos){
+         System.out.println("El numero uno dos es mayor");
+        } 
+        else {
+        System.out.println("El numero uno es menor que el dos");
+        }
+    } 
+ }  
